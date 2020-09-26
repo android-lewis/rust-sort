f93598d0a7ec681fc091af9e8de7c287204cf6de
Lewis Baston <lewis.baston@gmail.com>
2020-09-26 22:38:07 +0100

    Sorting out module structure

---
+++ src/example/sorts.rs
@@ -0,0 +1,61 @@
+use crate::example::helpers;
+
+pub fn do_hard_work(sort: &String) {
+    println!("{}", sort);
+    let mut array: [i32; 6] = [2, 4, 6, 12, 55, 13];
+    let _high = array.len() - 1;
+    let n = array.len();
+    //quick_sort(&mut array, 0, high);
+    bubble_sort(&mut array, n);
+    println!("{:?}", array);
+}
+
+pub fn quick_sort(array: &mut [i32], low: usize, high: usize) {
+    if low < high {
+        let pi = helpers::partition(array, low, high);
+        quick_sort(array, low, (pi - 1) as usize);
+        quick_sort(array, (pi + 1) as usize, high);
+    }
+}
+
+pub fn bubble_sort(array: &mut [i32], n: usize) {
+    for i in 0..(n - 1) {
+        let mut swapped = false;
+        for j in 0..(n - i - 1) {
+            if array[j] > array[j + 1] {
+                helpers::swap(array, j, j + 1);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break;
+        }
+    }
+}
+/*
+pub fn merge_sort(start: &[i32]) {
+
+}
+
+pub fn insertion_sort(start: &[i32]) {
+
+}
+
+pub fn selection_sort(start: &[i32]) {
+
+}
+
+pub fn heap_sort(start: &[i32]) {
+
+}
+
+pub fn radix_sort(start: &[i32]) {
+
+}
+
+pub fn bucket_sort(start: &[i32]) {
+
+}
+
+*/
