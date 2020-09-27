47eaf58fb23bbd237f59ba5092b3eba5e55ebd59
Lewis Baston <lewis.baston@gmail.com>
2020-09-27 14:26:46 +0100

    Implement insertion and merge sort

--- src/example/helpers.rs
+++ src/example/helpers.rs
@@ -20,8 +20,42 @@ where
     store_index
 }
 
-pub fn swap(param_int_array: &mut [i32], i: usize, j: usize) {
-    let temp = param_int_array[i];
-    param_int_array[i] = param_int_array[j];
-    param_int_array[j] = temp;
+pub fn merge(array: &mut [i32], l: usize, m: usize, r: usize) {
+    let n1: usize = m - l + 1;
+    let n2 = r - m;
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+    for i in 0..n1 {
+        left.push(array[l + i]);
+    }
+    for j in 0..n2 {
+        right.push(array[m + 1 + j]);
+    }
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = l;
+
+    while i < n1 && j < n2 {
+        if left[i] <= right[j] {
+            array[k] = left[i];
+            i += 1;
+        } else {
+            array[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while i < n1 {
+        array[k] = left[i];
+        i += 1;
+        k += 1;
+    }
+
+    while j < n2 {
+        array[k] = right[j];
+        j += 1;
+        k += 1;
+    }
 }
--- src/example/sorts.rs
+++ src/example/sorts.rs
@@ -1,33 +1,35 @@
 use crate::example::helpers;
 use crate::example::sort_types;
+use std::convert::TryInto;
 
 pub fn do_hard_work(sort: &sort_types::Sort) {
     println!("{:?}", sort);
     let mut array = [1, 0, 9, 8, 100, 345, 5, 6, 7, 4, 3, 2, 1];
+    println!("Before: {:?}\n", array);
     let n: usize = array.len();
     //let high = array.len() - 1;
     match sort {
-        sort_types::Sort::Quick => quick_sort(&mut array, &|x, y| x < y),
+        sort_types::Sort::Quick => quick_sort(&mut array, &|x, y| x > y),
         sort_types::Sort::Bubble => bubble_sort(&mut array, n),
-        sort_types::Sort::Merge => bubble_sort(&mut array, n),
-        sort_types::Sort::Insertion => bubble_sort(&mut array, n),
+        sort_types::Sort::Merge => merge_sort(&mut array, 0, n - 1),
+        sort_types::Sort::Insertion => insertion_sort(&mut array, n),
         sort_types::Sort::Selection => bubble_sort(&mut array, n),
         sort_types::Sort::Heap => bubble_sort(&mut array, n),
         sort_types::Sort::Radix => bubble_sort(&mut array, n),
         sort_types::Sort::Bucket => bubble_sort(&mut array, n),
     }
-    println!("After:  {:?}\n", array);
+    println!("After: {:?}\n", array);
 }
 
-pub fn quick_sort<T, F>(v: &mut [T], f: &F)
+pub fn quick_sort<T, F>(array: &mut [T], f: &F)
 where
     F: Fn(&T, &T) -> bool,
 {
-    let len = v.len();
+    let len = array.len();
     if len >= 2 {
-        let pivot_index = helpers::partition(v, f);
-        quick_sort(&mut v[0..pivot_index], f);
-        quick_sort(&mut v[pivot_index + 1..len], f);
+        let pivot_index = helpers::partition(array, f);
+        quick_sort(&mut array[0..pivot_index], f);
+        quick_sort(&mut array[pivot_index + 1..len], f);
     }
 }
 
@@ -36,7 +38,7 @@ pub fn bubble_sort(array: &mut [i32], n: usize) {
         let mut swapped = false;
         for j in 0..(n - i - 1) {
             if array[j] > array[j + 1] {
-                helpers::swap(array, j, j + 1);
+                array.swap(j, j + 1);
                 swapped = true;
             }
         }
@@ -46,15 +48,28 @@ pub fn bubble_sort(array: &mut [i32], n: usize) {
         }
     }
 }
+
+pub fn merge_sort(array: &mut [i32], l: usize, r: usize) {
+    if l < r {
+        let m = (l + (r - 1)) / 2;
+        merge_sort(array, l, m);
+        merge_sort(array, m + 1, r);
+        helpers::merge(array, l, m, r);
+    }
+}
+
+pub fn insertion_sort(array: &mut [i32], n: usize) {
+    for i in 1..n {
+        let key = array[i];
+        let mut j: isize = (i - 1).try_into().unwrap();
+        while j >= 0 && key < array[j as usize] {
+            array[(j + 1) as usize] = array[j as usize];
+            j -= 1;
+        }
+        array[(j + 1) as usize] = key;
+    }
+}
 /*
-pub fn merge_sort(start: &[i32]) {
-
-}
-
-pub fn insertion_sort(start: &[i32]) {
-
-}
-
 pub fn selection_sort(start: &[i32]) {
 
 }
